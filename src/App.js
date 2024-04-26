cd2dc48a642ec2bdfeced57858032028c5483483
charlx99 <charlotteuxd@gmail.com>
2024-04-26 13:16:05 +0800

    cleanup unwanted files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 import Weather from "./Weather";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello World!</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Weather city="Kyoto" />
-          Learn React
-        </a>
-      </header>
+      Meteo App
+      <Weather />
     </div>
   );
 }
-
-export default App;
