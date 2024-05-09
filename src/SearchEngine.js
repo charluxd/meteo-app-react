99f189e45b0b50dcfaefb2f5c41a1e270a480243
charlx99 <charlotteuxd@gmail.com>
2024-05-09 17:03:07 +0800

    Code Update (To be archived)

--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -3,146 +3,58 @@ import axios from "axios";
 import "./App.css";
 
 export default function SearchEngine() {
-  const [city, setCity] = useState("");
   const [loaded, setLoaded] = useState(false);
-  const [weather, setWeather] = useState({});
-  const [currentDate, setCurrentDate] = useState(getDate());
-
+  const [city, setCity] = useState(null);
   function displayWeather(response) {
+    setCity(response.data.main.temp);
     setLoaded(true);
-    setWeather({
-      temperature: response.data.main.temp,
-      description: response.data.weather[0].description,
-      humidity: response.data.main.humidity,
-      wind: response.data.wind.speed,
-      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
-    });
   }
-
-  function handleSubmit(event) {
-    event.preventDefault();
-    let apiKey = "a710bd8bd76400c9658ef649d9e81728";
-    let units = "metric";
-    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
-    axios.get(apiUrl).then(displayWeather);
-  }
-
-  function updateCity(event) {
-    setCity(event.target.value);
-  }
-
-  function getDate() {
-    let today = new Date();
-    // const day = today.getDay();
-    let hours = today.getHours();
-    let minutes = today.getMinutes();
-
-    let days = [
-      "Sunday",
-      "Monday",
-      "Tuesday",
-      "Wednesday",
-      "Thursday",
-      "Friday",
-      "Saturday",
-    ];
-    let day = days[today.getDay()];
-
-    if (hours < 10) {
-      hours = `0${hours}`;
-    }
-
-    if (minutes < 10) {
-      minutes = `0${minutes}`;
-    }
-
-    return `${day}, ${hours}:${minutes} `;
-  }
-
-  let form = (
-    <div className="weather">
-      <form onSubmit={handleSubmit}>
-        <input
-          type="search"
-          placeholder="Enter a City"
-          autoFocus={true}
-          class="input-field"
-          onChange={updateCity}
-        ></input>
-        <input type="submit" value="Search" class="button-search"></input>
-        <input type="submit" value="Current" class="button-current"></input>
-      </form>
-
-      {/* <div className="city-info">
-        <h2>Lisbon</h2>
-        <p>Monday 17:45</p>
-        <p>Clouds</p>
-        <img></img>
-      </div>
-      <div className="temperature-now">
-        <span className="temperature">
-          16
-          <small>°C</small>
-        </span>
-        <div>
-          <p>Precipitation: 55%</p>
-          <p>Wind: 9km/h</p>
-        </div>
-      </div> */}
-    </div>
-  );
 
   if (loaded) {
     return (
-      <div>
-        {form}
+      <div className="Weather">
+        <form>
+          <input
+            type="search"
+            placeholder="Enter a City"
+            autoFocus={true}
+            class="input-field"
+            // onChange={updateCity}
+          ></input>
+          <input type="submit" value="Search" class="button-search"></input>
+          <input type="submit" value="Current" class="button-current"></input>
+        </form>
+
         <div className="city-info">
           <h2>{city}</h2>
-          <p>{currentDate}</p>
-          <p>{weather.description}</p>
+          <p>9th May</p>
+          <p>Cloudy</p>
         </div>
-        <div className="temperature-now">
-          <span className="temperature">
-            <img src={weather.icon}></img>
-            {Math.round(weather.temperature)}
-            <small>°C</small>
-          </span>
-          <div>
-            <p>Humidity: {weather.humidity}%</p>
-            <p>Wind: {weather.wind}km/h</p>
+        <div className="row">
+          <div className="col-9 temperature-now">
+            <span>
+              <img
+                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+                className="temperature-icon"
+              ></img>
+            </span>
+            <span className="temperature-degree">15</span>
+            <span className="temperature-unit">°C</span>
           </div>
-        </div>
-
-        <div className="week-forecast">
-          <div className="daily-forecast">
-            <p>{setCurrentDate}</p>
-            <img src={weather.icon}></img>
-            <h3>{Math.round(weather.temperature)}°C</h3>
-          </div>
-          <div className="daily-forecast">
-            <p>{setCurrentDate}</p>
-            <img src={weather.icon}></img>
-            <h3>{Math.round(weather.temperature)}°C</h3>
-          </div>
-          <div className="daily-forecast">
-            <p>{setCurrentDate}</p>
-            <img src={weather.icon}></img>
-            <h3>{Math.round(weather.temperature)}°C</h3>
-          </div>
-          <div className="daily-forecast">
-            <p>{setCurrentDate}</p>
-            <img src={weather.icon}></img>
-            <h3>{Math.round(weather.temperature)}°C</h3>
-          </div>
-          <div className="daily-forecast">
-            <p>{setCurrentDate}</p>
-            <img src={weather.icon}></img>
-            <h3>{Math.round(weather.temperature)}°C</h3>
+          <div className="col-3">
+            <p>Humidity: 15%</p>
+            <p>Wind: 73km/h</p>
           </div>
         </div>
       </div>
     );
   } else {
-    return form;
+    const apiKey = "2ff29bed3181c3526c35cc5408037f85";
+    let city = "New York";
+    let units = "metric";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${units}`;
+    axios.get(apiUrl).then(displayWeather);
+
+    return "Loading...";
   }
 }
